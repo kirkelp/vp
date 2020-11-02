02884b4f3129a4e7d8c1f13a30525901ad9ebd1e
Kirke Liis Paur <kirkelp@users.noreply.github.com>
2020-11-02 14:49:55 +0200

    tun9 ja kodunetöö

---
+++ tund9/addfilmrelations.php
@@ -0,0 +1,161 @@
+<?php
+
+session_start();
+//kui pole sisselogitud
+if(!isset($_SESSION["userid"])){
+	//jõuga
+	header("Location: page.php");
+}
+//väljalogimine
+if(isset($_GET["logout"])){
+	session_destroy();
+	header("Location: page.php");
+	exit();
+}
+
+require("fnc_filmrelations.php");
+require("../../../config.php");
+  
+   $genrenotice = "";
+    $studionotice = "";
+    $selectedfilm = "";
+    $selectedgenre = "";
+    $selectedstudio = "";
+    $personnotice = "";
+    $selectedperson = "";
+    $selectedrole = "";
+    $selectedposition = "";
+    $role = "";
+    $quotenotice = "";
+    $selectedquote = "";
+    $selectedmovieperson = "";
+
+    if(isset($_POST["filmrelationsubmit"])){
+      //$selectedfilm = $_POST["filminput"];
+      if(!empty($_POST["filminput"])){
+          $selectedfilm = intval($_POST["filminput"]);
+      } else {
+          $genrenotice = " Vali film!";
+      }
+      if(!empty($_POST["filmgenreinput"])){
+          $selectedgenre = intval($_POST["filmgenreinput"]);
+      } else {
+          $genrenotice .= " Vali žanr!";
+      }
+      if(!empty($selectedfilm) and !empty($selectedgenre)){
+          $genrenotice = storenewgenrerelation($selectedfilm, $selectedgenre);
+      }
+    }
+
+    if(isset($_POST["filmstudiorelationsubmit"])){
+        if(!empty($_POST["filminput"])){
+            $selectedfilm = intval($_POST["filminput"]);
+        } else {
+            $studionotice = " Vali Film!";
+        }
+        if(!empty($_POST["filmstudiosinput"])){
+            $selectedstudio = intval($_POST["filmstudiosinput"]);
+        } else {
+            $studionotice .= " Vali Stuudio!";
+        }
+        if(!empty($selectedfilm) and !empty($selectedstudio)){
+            $studionotice = storenewstudiorelation($selectedfilm, $selectedstudio);
+        }
+    }
+
+    if(isset($_POST["personinmovierelationsubmit"])){
+        if(!empty($_POST["filminput"])){
+            $selectedfilm = intval($_POST["filminput"]);
+        } else {
+            $personnotice .= " Vali Film!";
+        }
+        if(!empty($_POST["personinput"])){
+            $selectedperson = intval($_POST["personinput"]);
+        } else {
+            $personnotice .= " Vali Isik!";
+        }
+        if(!empty($_POST["positioninput"])){
+            $selectedposition = intval($_POST["positioninput"]);
+        } else {
+            $personnotice .= " Vali Ametikoht!";
+        }
+        if(!empty($_POST["roleinput"])){
+            $selectedrole = test_input($_POST["roleinput"]);
+        } elseif($_POST["positioninput"] === "2") {
+            $selectedrole = "Režissöör";
+        } else {
+            $personnotice .= " Lisa Roll!";
+        }
+        if(!empty($selectedfilm) and !empty($selectedperson) and !empty($selectedposition) and !empty($selectedrole)){
+            $personnotice = storenewpersonrelation($selectedfilm, $selectedperson, $selectedposition, $selectedrole);
+        }
+    }
+
+    if(isset($_POST["quoterelationsubmit"])){
+        if(!empty($_POST["personinmovieinput"])){
+            $selectedmovieperson = intval($_POST["personinmovieinput"]);
+        } else {
+            $quotenotice .= " Vali Isik, roll, film!";
+        }
+        if(!empty($_POST["quoteinput"])){
+            $selectedquote = test_input($_POST["quoteinput"]);
+        } else {
+            $quotenotice .= "Lisa tsitaat!";
+        }
+        if(!empty($selectedmovieperson) and !empty($selectedquote)){
+            $quotenotice = storenewquoterelation($selectedmovieperson, $selectedquote);
+        }
+    }
+    
+    $filmselecthtml = readmovietoselect($selectedfilm);
+    $filmgenreselecthtml = readgenretoselect($selectedgenre);
+    $filmstudiotoselecthtml = readstudiotoselect($selectedstudio);
+    $personinmovieselecthtml = readpersontoselect($selectedperson);
+    $positionselecthtml = readpositiontoselect($selectedposition);
+    $moviepersonselecthtml = readpersoninmovietoselect($selectedmovieperson);
+	
+	require("header.php");
+?>
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse bänner">
+  <h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?> programmeerib veebi</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+    <ul>
+    <li><a href="home.php">Avalehele</a></li>
+	<li><a href="?logout=1">Logi välja</a>!</li>
+  </ul>
+        <form class="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label>Seos filmi ja zanri vahel: </label>
+            <?php
+                echo $filmselecthtml;
+                echo $filmgenreselecthtml;
+            ?>
+        <input type="submit" name="filmrelationsubmit" value="Salvesta"><span><?php echo $genrenotice; ?></span>
+        <hr>
+        <form class="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label>Seos filmi ja stuudio vahel: </label>
+            <?php
+                echo $filmselecthtml;
+                echo $filmstudiotoselecthtml;
+            ?>
+        <input type="submit" name="filmstudiorelationsubmit" value="Salvesta"><span><?php echo $studionotice; ?></span>
+        <hr>
+        <form class="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label>Seos filmi ja isiku vahel: </label>
+            <?php
+                echo $filmselecthtml;
+                echo $personinmovieselecthtml;
+                echo $positionselecthtml;
+            ?>
+        <input type="text" name="roleinput" placeholder="Roll">
+        <input type="submit" name="personinmovierelationsubmit" value="Salvesta"><span><?php echo $personnotice; ?></span>
+        <hr>
+        <form class="form" method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+            <label>Tsitaat: </label>
+            <?php
+                echo $moviepersonselecthtml;
+            ?>
+            <input type="text" name="quoteinput" placeholder="Tsitaat">
+        <input type="submit" name="quoterelationsubmit" value="Salvesta"><span><?php echo $quotenotice; ?></span>
+    </body>
+</html>
--- tund9/photoupload.php
+++ tund9/photoupload.php
@@ -33,96 +33,97 @@ require("fnc_common.php");
   if(isset($_POST["photosubmit"])){
 	//var_dump($_POST);
 	//var_dump($_FILES);
+	$alttext = test_input($_POST["altinput"]);
+	$privacy = intval($_POST["privinput"]);
 	//kas on üldse pilt
-	$check = getimagesize($_FILES["photoinput"]["tmp_name"]);
-	//var_dump($check);
-	if($check !== false){
-		if($check["mime"] == "image/jpeg"){
-			$filetype = "jpg";
-		}
-		if($check["mime"] == "image/png"){
-			$filetype = "png";
-		}
-		if($check["mime"] == "image/gif"){
-			$filetype = "gif";
-		}
-	} else {
-		$error = "Valitud fail ei ole pilt!";
-	}
-	
-	//pildi suurus
-	if($_FILES["photoinput"]["size"] > 1048576){
-		$error .= " Fail ületab lubatud suuruse!";
-	}
-	
-	//loon failinime
-	$timestamp = microtime(1) * 10000;
-	$filename = $filenameprefix .$timestamp ."." .$filetype;
-	
-	//kas on juba olemas
-	if(file_exists($origphotodir .$filename)){
-		$error .= " Selle nimega pildifail on juba olemas!";
-	}
-	
-	if(empty($error)){
-		//teeme pildi väiksemaks
-		//teeme pildiobjekti - pikslikogumi
-		if($filetype == "jpg"){
-			$mytempimage = imagecreatefromjpeg($_FILES["photoinput"]["tmp_name"]);
-		}
-		if($filetype == "png"){
-			$mytempimage = imagecreatefrompng($_FILES["photoinput"]["tmp_name"]);
-		}
-		if($filetype == "gif"){
-			$mytempimage = imagecreatefromgif($_FILES["photoinput"]["tmp_name"]);
-		}
-		//muudame pildi suurust
-		$mynewimage = resizePhoto($mytempimage, $maxphotowidth, $maxphotoheight, true);
-		
-		$result = savePhotoFile($mynewimage, $filetype, $normalphotodir .$filename);
-		if($result == 1){
-			$notice .= "Vähendatud pildi salvestamine õnnestus!";
-		} else {
-			$error .= "Vähendatud pildi salvestamisel tekkis tõrge!";
-		}
-		
-		imagedestroy($mynewimage);
-		
-		//teeme pisipildi
-		$mynewimage = resizePhoto($mytempimage, $thumbsize, $thumbsize);
-		
-		$result = savePhotoFile($mynewimage, $filetype, $thumbphotodir .$filename);
-		if($result == 1){
-			$notice .= "Pisipildi salvestamine õnnestus!";
-		} else {
-			$error .= "Pisipildi salvestamisel tekkis tõrge!";
-		}
-		
-		if(empty($error)){
-			if(move_uploaded_file($_FILES["photoinput"]["tmp_name"], $origphotodir .$filename)){
-				$notice .= " Originaalfaili üleslaadimine õnnestus!";
-			} else {
-				$error .= " Originaalfaili üleslaadimisel tekkis tõrge!";
+	if(isset($_FILES["photoinput"]["tmp_name"])){
+		$check = getimagesize($_FILES["photoinput"]["tmp_name"]);
+		//var_dump($check);
+		if($check !== false){
+			if($check["mime"] == "image/jpeg"){
+				$filetype = "jpg";
 			}
+			if($check["mime"] == "image/png"){
+				$filetype = "png";
+			}
+			if($check["mime"] == "image/gif"){
+				$filetype = "gif";
+			}
+		} else {
+			$error = "Valitud fail ei ole pilt!";
+		}
+		
+		//pildi suurus
+		if($_FILES["photoinput"]["size"] > 1048576){
+			$error .= " Fail ületab lubatud suuruse!";
+		}
+		
+		//loon failinime
+		$timestamp = microtime(1) * 10000;
+		$filename = $filenameprefix .$timestamp ."." .$filetype;
+		
+		//kas on juba olemas
+		if(file_exists($origphotodir .$filename)){
+			$error .= " Selle nimega pildifail on juba olemas!";
 		}
 		
 		if(empty($error)){
-			$privacy = intval($_POST["privinput"]);
-			$alttext = test_input($_POST["altinput"]);
-			$result = storePhotoData($filename, $alttext, $privacy);
+			//teeme pildi väiksemaks
+			//teeme pildiobjekti - pikslikogumi
+			if($filetype == "jpg"){
+				$mytempimage = imagecreatefromjpeg($_FILES["photoinput"]["tmp_name"]);
+			}
+			if($filetype == "png"){
+				$mytempimage = imagecreatefrompng($_FILES["photoinput"]["tmp_name"]);
+			}
+			if($filetype == "gif"){
+				$mytempimage = imagecreatefromgif($_FILES["photoinput"]["tmp_name"]);
+			}
+			//muudame pildi suurust
+			$mynewimage = resizePhoto($mytempimage, $maxphotowidth, $maxphotoheight, true);
+			
+			$result = savePhotoFile($mynewimage, $filetype, $normalphotodir .$filename);
 			if($result == 1){
-				$notice .= " Pildi info lisati andmebaasi!";
+				$notice .= "Vähendatud pildi salvestamine õnnestus!";
 			} else {
-				$error .= "Pildi info andmebaasi salvestamisel tekkis tõrge!";
+				$error .= "Vähendatud pildi salvestamisel tekkis tõrge!";
 			}
-		} else {
-			$error .= " Tekkinud vigade tõttu pildi andmeid ei salvestatud!";
+			
+			imagedestroy($mynewimage);
+			
+			//teeme pisipildi
+			$mynewimage = resizePhoto($mytempimage, $thumbsize, $thumbsize);
+			
+			$result = savePhotoFile($mynewimage, $filetype, $thumbphotodir .$filename);
+			if($result == 1){
+				$notice .= "Pisipildi salvestamine õnnestus!";
+			} else {
+				$error .= "Pisipildi salvestamisel tekkis tõrge!";
+			}
+			
+			if(empty($error)){
+				if(move_uploaded_file($_FILES["photoinput"]["tmp_name"], $origphotodir .$filename)){
+					$notice .= " Originaalfaili üleslaadimine õnnestus!";
+				} else {
+					$error .= " Originaalfaili üleslaadimisel tekkis tõrge!";
+				}
+			}
+			
+			if(empty($error)){
+				$result = storePhotoData($filename, $alttext, $privacy);
+				if($result == 1){
+					$notice .= " Pildi info lisati andmebaasi!";
+				} else {
+					$error .= "Pildi info andmebaasi salvestamisel tekkis tõrge!";
+				}
+			} else {
+				$error .= " Tekkinud vigade tõttu pildi andmeid ei salvestatud!";
+			}
+		imagedestroy($mytempimage);
 		}
-	imagedestroy($mytempimage);
-	}
-		
+			
+	  }
   }
-
   require("header.php");
 ?>
 
